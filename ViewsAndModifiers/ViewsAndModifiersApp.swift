c6375d937b03c5c3bc86c137b2f6818e00a86101
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2020-12-23 11:00:33 -0800

    First commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Arshya GHAVAMI on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
