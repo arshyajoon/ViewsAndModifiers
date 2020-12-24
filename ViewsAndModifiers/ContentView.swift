898def63e01a9e4dc1ec462412a69149af87d2b9
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2020-12-24 12:54:23 -0800

    Created a ViewModifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+            .titleStyle()
             .padding()
             .background(Color.red)
             .padding()
@@ -20,6 +21,20 @@ struct ContentView: View {
             .background(Color.yellow)
     }
 }
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
